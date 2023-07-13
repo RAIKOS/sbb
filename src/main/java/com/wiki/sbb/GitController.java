51e07e9fe1f8021d76d6c64f4fbbb99682d00baa
RAIKOS <plha.amazon@gmail.com>
2023-07-13 16:22:27 +0900

    푸시......... 좀 되라.....

--- src/main/java/com/wiki/sbb/GitController.java
+++ src/main/java/com/wiki/sbb/GitController.java
@@ -8,9 +8,8 @@ public class GitController {
 
 	public static void main(String[] args) {
 		
-		String git = "된건가";
-		String token = "모르겠네";
-		String token1 = "1111111모르겠네";
+		String git = "푸시";
+		String token = "이클립스";
 		
 		
 		SpringApplication.run(SbbApplication.class, args);
