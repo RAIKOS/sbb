01d7444d2209ce0a7fa484c91ec9b09970a69839
RAIKOS <plha.amazon@gmail.com>
2023-07-13 15:41:08 +0900

    오류 수정

--- src/main/java/com/wiki/sbb/GitController.java
+++ src/main/java/com/wiki/sbb/GitController.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbbApplication {
+public class GitController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbbApplication.class, args);
