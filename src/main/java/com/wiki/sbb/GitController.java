16847b7ada72972e4096f5a6dde5397d9b47a145
RAIKOS <plha.amazon@gmail.com>
2023-07-13 15:44:36 +0900

    푸시 테스트

--- src/main/java/com/wiki/sbb/GitController.java
+++ src/main/java/com/wiki/sbb/GitController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitController {
 
 	public static void main(String[] args) {
+		// 푸시는 어케하는거냐...
 		SpringApplication.run(SbbApplication.class, args);
 	}
 
